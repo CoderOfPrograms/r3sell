ff29fadef8699ded48c7cc111d660a5f3e5a08c9
Gerald Lu <lujerry131@gmail.com>
2023-11-18 21:47:39 -0500

    json file formatted correctly and README.md updated

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # r3sell
 AI Atlanta Hackathon project to enable the resale economy
+
+
+# To Run: 
+```
+python3 -m streamlit run app.py
+```
+
--- app.py
+++ app.py
@@ -8,7 +8,11 @@ from llava import get_quality
 from os.path import abspath
 import os
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
-
+import re
+def convert_string_to_dict(data_string):
+    pairs = re.findall(r'"([^"]+)"\s*:\s*"([^"]*)"', data_string)
+    data_dictionary = dict(pairs)
+    return data_dictionary
 
 def improving_picture(file_name):
     image = file_name.getvalue()
@@ -29,7 +33,15 @@ def add_white_background_to_image_bytes(modified_image_bytes):
     new_image.save(output_image_bytes, format='PNG')  # Change format if needed
     
     return output_image_bytes.getvalue()
-pipeline = pipeline(task="image-classification", model="julien-c/hotdog-not-hotdog")
+def find_brackets_indices(s):
+    first_open_bracket = s.find("{")
+    
+    if first_open_bracket != -1:
+        second_last_close_bracket = s.rfind("}", first_open_bracket + 1, len(s) - 1)
+        return first_open_bracket, second_last_close_bracket
+    else:
+        return -1, -1  # Return -1 if "{" is not found
+#pipeline = pipeline(task="image-classification", model="julien-c/hotdog-not-hotdog")
 
 # Set the title and text color to dark green
 st.markdown('<h1 style="color:darkgreen;">R3SELL</h1>', unsafe_allow_html=True)
@@ -41,8 +53,13 @@ file_name = st.file_uploader("Upload an image file (JPEG, PNG, etc.)", type=["pn
 image = st.camera_input("Capture an image from your webcam")
 
 # Add a text bar to add a title
-#image_title = st.text_input("Image Title", value="Specificity is nice!")
-st.write('Prices: ' + str(search_ebay_sold_items(st.text_input("Image Title", value="Specificity is nice!"))))
+image_title = st.text_input("Image Title")
+price = 'Prices: '
+if image_title != '':
+    price = 'Prices: ' + str(search_ebay_sold_items(image_title))
+    st.write(price)
+else:
+    st.write(price)
 
 # Add a text bar to add a description
 #mage_description = st.text_input("Image Description", value="(Optional)")
@@ -65,26 +82,21 @@ if file_name is not None or image is not None:
                     f.write(uploaded_file.getbuffer())
         save_uploaded_image_locally(file_name)
         st.write('Description: ' + get_quality(abspath(nameFile))[5:])
-
+        st.write('')
+        st.write('<span style="font-size: 36px;">Modified Image with White Background </span>', unsafe_allow_html=True)
         image_rem = add_white_background_to_image_bytes(improving_picture(file_name))
 
-    # Pass the captured image to the pipeline function
-    predictions = pipeline(image)
-
-    col1, col2 = st.columns(2)
 
 
-    col1.image(image_rem, use_column_width=True)
 
-    col2.header("Probabilities")
-    for p in predictions:
-        col2.subheader(f"{ p['label'] }: { round(p['score'] * 100, 1)}%")
+    st.image(image_rem, use_column_width=True)
+
 anthropic = Anthropic(
     api_key="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",
 )
-name = "Macbook Pro M1"
+name = str(image_title)
 description = "get_quality(abspath(nameFile))[5:]"
-price = str(search_ebay_sold_items(st.text_input("Image Title", value="Specificity is nice!")))
+output_price = str(price)
 s = "Name of Product is "+ name +". Min, 1st quartile, median, 3rd quartile, and max of used prices are "+ price +". Assume max is best used quality and min in worst used quality. Color and defects(consider in price and description) -  "+description+"(sub color with official colorname). Generate a price given previous which ends in .99. Feel free to search the web. Video should be a product/review video make sure video is real. Generate a product description and the relevant information for an ebay listing. Search the web for additional help and it should be good for the ebay algorithms. Give it as a JSON with these categories Product Name, Price, Condition, Brand, Type, Color, Description, Specifications, Size, Video, Department. Generate Facebook and Instagram ads with hashtags and other stuff to boost algorithm. "
 promp = "\n\nHuman ${userQuestion}\n\nAssistant:"
 completion = anthropic.completions.create(
@@ -92,4 +104,9 @@ completion = anthropic.completions.create(
     max_tokens_to_sample=1000,
     prompt=f"{HUMAN_PROMPT} "+s+f" {AI_PROMPT}",
 )
-st.write(completion.completion)
+first_index, second_to_last_index = find_brackets_indices(completion.completion)
+string_data = completion.completion[first_index:second_to_last_index + 1]
+print(list(convert_string_to_dict(string_data).values()))
+for key, value in convert_string_to_dict(string_data).items():
+    st.write(f'{key}: {value}')
+st.write(completion.completion[second_to_last_index + 1:])
